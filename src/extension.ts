64e5f1a568cb6a15e212731a35306d68bf68cf3f
GongXian Cao <gongxian.cgx@alibaba-inc.com>
2024-05-30 10:19:06 +0800

    Show hello world webview

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,40 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vss-visualizer" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vss-visualizer" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vss-visualizer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSS Visualizer!');
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('vss-visualizer.visualize', () => {
+        // Create and show a new webview
+        const panel = vscode.window.createWebviewPanel(
+            'catCoding', // Identifies the type of the webview. Used internally
+            'Cat Coding', // Title of the panel displayed to the user
+            vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+            {} // Webview options. More on these later.
+        );
 
-	context.subscriptions.push(disposable);
+        panel.webview.html = getWebviewContent();
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+    </head>
+    <body>
+        hello world
+    </body>
+    </html>`;
 }
 
 // This method is called when your extension is deactivated
