2f27cc091e6db231a82dd31dc06f0c6f4ebaf3a0
GongXian Cao <gongxian.cgx@alibaba-inc.com>
2024-06-11 13:15:08 +0800

    set node font size based on leaves the subtree has.

--- media/vss-visualizer.js
+++ media/vss-visualizer.js
@@ -36,6 +36,7 @@
     var radius = 10;
     var indent = 100 * radius;
     var fontSize = 100;
+    var leafFontSize = 30;
     var nodePaddingX = radius;
     var nodePaddingY = radius;
     var zoomed = false;
@@ -74,25 +75,44 @@
         'actuator': '#f00',
     };
 
-    function layoutVssNode(node, level, left, top) {
-        var right = left + 2 * radius + 2 * nodePaddingX;
-        var bottom = top + 2 * radius + 2 * nodePaddingY;
-        var x = left + nodePaddingX + radius;
-        var y = top + nodePaddingY + radius;
-        var children = [];
-        var levelIndent = indent / Math.pow(2, level * 0.3);
-
-        var nextTop = top;
+    function countLeaves(node) {
         if (node.children) {
+            let count = 0;
             for (var child of node.children) {
-                var layout = layoutVssNode(child, level + 1, left + levelIndent, nextTop);
+                count += countLeaves(child);
+            }
+            return count;
+        } else {
+            return 1;
+        }
+    }
+
+    
+    function layoutVssNode(node, level, left, top) {
+        let right = left + 2 * radius + 2 * nodePaddingX;
+        let bottom = top + 2 * radius + 2 * nodePaddingY;
+        let x = left + nodePaddingX + radius;
+        let y = top + nodePaddingY + radius;
+        let children = [];
+        let levelIndent = indent / Math.pow(2, level * 0.3);
+
+        let nextTop = top;
+        let leafCount = 0;
+        if (node.children) {
+            leafCount = countLeaves(node);
+            let childIndent = left + levelIndent + radius * 2 * leafCount;
+            for (var child of node.children) {
+                var layout = layoutVssNode(child, level + 1, childIndent, nextTop);
                 nextTop = layout.bottom;
                 right = Math.max(layout.right, right);
                 children.push(layout);
             }
 
             y = children.length ? Math.max((children[0].y + children[children.length - 1].y) / 2, y) : y;
+        } else {
+            leafCount = 1;
         }
+
         bottom = Math.max(bottom, nextTop);
         return {
             x,
@@ -102,7 +122,8 @@
             top,
             bottom,
             node,
-            children
+            children,
+            leafCount
         };
     }
 
@@ -132,8 +153,6 @@
                 ctx.lineTo(end.x, end.y);
                 ctx.strokeStyle = edgeColor;
                 ctx.stroke();
-
-                drawLayoutedVssNode(ctx, transform, level + 1, child);
             }
         }
         let transformedRadius = transformLength(radius, transform);
@@ -144,15 +163,23 @@
 
         let showLabel = shouldShowLabel(level);
         if (showLabel) {
-            let levelFontSize = fontSize / Math.pow(2, level * 0.3) * transform.k;
-            let namePosition = {
-                x: layoutedNode.x,
-                y: layoutedNode.y
-            };
-    
-            let transformedNamePosition = transformPosition(namePosition, transform);
-            ctx.font = `${levelFontSize}px sans-serif`;
-            ctx.fillText(layoutedNode.node.name, transformedNamePosition.x + levelFontSize, transformedNamePosition.y + levelFontSize / 3);
+            let nodeFontSize = leafFontSize * Math.pow(layoutedNode.leafCount, 0.7) * transform.k;
+            if (nodeFontSize > 2) {
+                let namePosition = {
+                    x: layoutedNode.x + radius * 1,
+                    y: layoutedNode.y
+                };
+        
+                let transformedNamePosition = transformPosition(namePosition, transform);
+                ctx.font = `${nodeFontSize}px sans-serif`;
+                ctx.fillText(layoutedNode.node.name, transformedNamePosition.x + nodeFontSize / 2, transformedNamePosition.y + nodeFontSize / 3);
+            }
+        }
+
+        if (layoutedNode.children) {
+            for (var child of layoutedNode.children) {
+                drawLayoutedVssNode(ctx, transform, level + 1, child);
+            }
         }
     }
 
